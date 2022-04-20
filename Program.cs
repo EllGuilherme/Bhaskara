60b1f46e9631da8706c8b583ea8a7d26bc2429ec
EllGuilherme <grds1000@gmail.com>
2022-04-19 23:24:22 -0300

    finalizar tarefa 2.0

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ a = Convert.ToDouble(Console.ReadLine());
      Console.Write("(b): ");
 b = Convert.ToDouble(Console.ReadLine());
 
-    Console.Write("(c) : ");
+     Console.Write("(c): ");
 c = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine();
@@ -26,14 +26,18 @@ else
  {
  delta = b * b - 4 * a * c;
 
- if (a <0)
+ if (delta <0)
 {
-Console.WriteLine("$Como delta = {delta:N2}, a equação não possui raízes reais!");
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"Como delta = {delta:N2}, a equação não possui raízes reais!");
+    Console.ResetColor();
 }
 else
 {
     x1 = (-b + Math.Sqrt(delta)) / (2 * a);
     x2 = (-b - Math.Sqrt(delta)) / (2 * a);
-  Console.WriteLine($"x1 = {x1:N2} e x2 = {x2:N2}");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine($"x1 = {x1:N2} e x2 = {x2:N2}"); 
+    Console.ResetColor();
 }
 }
